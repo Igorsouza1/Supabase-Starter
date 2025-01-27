338d8392f6d6d4cef78faa496acb97b752cd625e
igor <igor1_souza@hotmail.com>
2025-01-27 15:16:07 -0400

    Limpeza do modelo supabase

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -10,26 +10,26 @@ export default async function ResetPassword(props: {
   const searchParams = await props.searchParams;
   return (
     <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
-      <h1 className="text-2xl font-medium">Reset password</h1>
+      <h1 className="text-2xl font-medium">Resetar a senha</h1>
       <p className="text-sm text-foreground/60">
-        Please enter your new password below.
+        Por favor escolha sua nova senha a baixo
       </p>
-      <Label htmlFor="password">New password</Label>
+      <Label htmlFor="password">Nova Senha</Label>
       <Input
         type="password"
         name="password"
-        placeholder="New password"
+        placeholder="Nova password"
         required
       />
-      <Label htmlFor="confirmPassword">Confirm password</Label>
+      <Label htmlFor="confirmPassword">Confirmar Nova Senha</Label>
       <Input
         type="password"
         name="confirmPassword"
-        placeholder="Confirm password"
+        placeholder="Confirme a senha"
         required
       />
       <SubmitButton formAction={resetPasswordAction}>
-        Reset password
+        Resetar Senha
       </SubmitButton>
       <FormMessage message={searchParams} />
     </form>
